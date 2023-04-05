3f1192a27c13e7c6cc12af9aa754d880401503bd
Anna7682 <107514746+Anna7682@users.noreply.github.com>
2023-04-05 20:58:57 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./css/style.css";
+import "./js/app";
+import './images/americanexpress.png';
+import "./images/dinersclub.png";
+import "./images/discover.png";
+import "./images/jcb.png";
+import "./images/mastercard.png";
+import "./images/mir.png";
+import './images/visa.png';
+import "./license.txt";
