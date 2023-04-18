896db913f129199708136456d11c1593a1d8678a
Anna7682 <107514746+Anna7682@users.noreply.github.com>
2023-04-18 11:45:56 +0300

    Delete app.js

--- src/js/app.js
+++
@@ -1,7 +0,0 @@
-import Widget from './Widget';
-import Validator from './Validator';
-import Identificator from './Identificator';
-
-new Widget();
-Validator.Validate('widget-card-input');
-Identificator.Identificate('widget-card-input');
