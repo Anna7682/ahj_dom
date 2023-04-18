e545f1aa62cc0634a3ca1973984e1051034960c3
Anna7682 <107514746+Anna7682@users.noreply.github.com>
2023-04-18 11:37:11 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
-import "./css/style.css";
-import "./js/app";
-import './images/americanexpress.png';
-import "./images/dinersclub.png";
-import "./images/discover.png";
-import "./images/jcb.png";
-import "./images/mastercard.png";
-import "./images/mir.png";
-import './images/visa.png';
-import "./license.txt";
+import './css/style.css';
+import './js/app';
+
+// entry point for webpack
+// don't write your code here
